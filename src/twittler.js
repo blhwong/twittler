e974c47c56bc1b74e03527401536bee40b19d8e3
Brandon Wong <b.lh.wong@gmail.com>
2017-01-24 20:03:14 -0800

    Timestamps working and new tweets populating at top

--- src/twittler.js
+++ src/twittler.js
@@ -1,13 +1,26 @@
 $(document).ready(function(){
   var $body = $('body');
-  $body.html('<br><br><br><br><br><br><br><br>');
+  $body.html('<br><br><br><br><button type="button">Show New Tweets</button><br><br><br><br>');
 
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    var tweet = streams.home[index];
-    var $tweet = $('<div></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($body);
-    index -= 1;
+
+  $('button').on('click', showTweets);
+
+  showTweets();
+  function showTweets() {
+    //$body.html('<br><br><br><br><button type="button">Show New Tweets</button><br><br><br><br>');
+    $('.tweet').remove();
+    var index = streams.home.length - 1;
+    console.log(index);
+    while(index >= 0){
+      var tweet = streams.home[index];
+      var $tweet = $('<div class="tweet"></div>');
+      var d = tweet.created_at;
+      var time = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+
+      $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + time);
+      $tweet.appendTo($body);
+      index -= 1;
+    }
+
   }
 });
