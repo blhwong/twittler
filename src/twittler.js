9a52adbba879bae4df07c9ae1a47b3b127a46971
Brandon Wong <b.lh.wong@gmail.com>
2017-01-24 23:34:13 -0800

    In process of adding user timeline on click

--- src/twittler.js
+++ src/twittler.js
@@ -7,20 +7,36 @@ $(document).ready(function(){
 
   showTweets();
   function showTweets() {
-    //$body.html('<br><br><br><br><button type="button">Show New Tweets</button><br><br><br><br>');
     $('.tweet').remove();
     var index = streams.home.length - 1;
     console.log(index);
     while(index >= 0){
       var tweet = streams.home[index];
       var $tweet = $('<div class="tweet"></div>');
+      var $name = $('<div class="user"></div>');
+      $('.user').click(function() {
+        //console.log(user);
+        $('body').html('');
+        var $timeline = $('<div class="timeline"></div>');
+        $timeline.text(streams.users['mracus']);
+        $timeline.appendTo($body);
+      });
       var d = tweet.created_at;
       var time = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-
-      $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + time);
+      //var $user = $('<div><button class="user">@ +tweet.user+ </button></div>');
+      //$tweet.append($user);
+      $name.text('@' + tweet.user);
+      $tweet.text(': ' + tweet.message + ' ' + time);
+      $name.appendTo($tweet);
       $tweet.appendTo($body);
       index -= 1;
     }
+
+  }
+
+  function showUser(username) {
+    $body.html('');
+    var index = streams.users[].length - 1;
 
   }
 });
