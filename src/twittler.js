79961bbe023727e6e6de1fdde5238b7c873a4a39
Brandon Wong <b.lh.wong@gmail.com>
2017-01-25 11:20:03 -0800

    Prepend anchor tag to username

--- src/twittler.js
+++ src/twittler.js
@@ -1,13 +1,9 @@
 $(document).ready(function(){
   var $body = $('body');
   $body.html('<br><br><br><br><button type="button">Show New Tweets</button><br><br><br><br>');
-
-
   $('button').on('click', showTweets);
-
   showTweets();
   function showTweets() {
-    //$body.html('<br><br><br><br><button type="button">Show New Tweets</button><br><br><br><br>');
     $('.tweet').remove();
     var index = streams.home.length - 1;
     console.log(index);
@@ -16,11 +12,10 @@ $(document).ready(function(){
       var $tweet = $('<div class="tweet"></div>');
       var d = tweet.created_at;
       var time = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-
-      $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + time);
+      $tweet.text(': ' + tweet.message + ' ' + time);
+      $tweet.prepend($('<a>@'+tweet.user+'</a>'));
       $tweet.appendTo($body);
       index -= 1;
     }
-
   }
 });
